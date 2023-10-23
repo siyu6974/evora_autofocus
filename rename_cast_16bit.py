f56d5b0aa383dbf5f2b2e5e5b5652a6886563bdd
Siyu Zhang <astro6974@gmail.com>
2023-10-22 21:04:39 -0700

    Enable batch conversion for rename_cast_16bit.py
    python rename_cast_16bit.py  /data/ecam/20231021/*.fits

--- rename_cast_16bit.py
+++ rename_cast_16bit.py
@@ -14,18 +14,23 @@ def rename_file(directory, old_name, new_name):
     shutil.move(old_path, new_path)
     print(f"Renamed '{old_name}' to '{new_name}'.")
 
-filename = sys.argv[1]
-hdul = fits.open(filename)
-h = hdul[0].header
-image_data = hdul[0].data
 
-seq_nb = filename.split("/")[-1].split("-")[-1].split(".")[0]
-temp = h["TEMP"]
-filter_exp = h["FILTER"]
-exp_time = h["EXP_TIME"]
-date_obs = h["DATE-OBS"].split(".")[0]
-new_name = f"{date_obs}_{filter_exp}_{temp}_{exp_time}s_{seq_nb}.fits"
-print(new_name)
-hdu = fits.PrimaryHDU(data=image_data.astype(np.uint16))
-new_name = new_name.replace(":", "-")
-hdu.writeto(new_name)
+for filepath in sys.argv[1:]:
+    hdul = fits.open(filepath)
+    h = hdul[0].header
+    image_data = hdul[0].data
+
+    directory = os.path.dirname(filepath)
+    seq_nb = os.path.basename(filepath).split("-")[-1].split(".")[0]
+    temp = h["TEMP"]
+    filter_exp = h["FILTER"]
+    exp_time = h["EXP_TIME"]
+    date_obs = h["DATE-OBS"].split(".")[0]
+    new_name = f"{date_obs}_{filter_exp}_{temp}_{exp_time}s_{seq_nb}.fits"
+    new_name = new_name.replace(":", "-")
+    print(new_name)
+    new_path = f"{directory}/{new_name}"
+
+    # convert to 16 bit
+    hdu = fits.PrimaryHDU(data=image_data.astype(np.uint16))
+    hdu.writeto(new_path)
